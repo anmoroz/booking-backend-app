f8f86e23f632397b790447165b270280a143817d
Morozov Andrey <andrey@3davinci.ru>
2022-09-21 08:47:37 +0300

    Added copyright

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * This file is part of the Booking application project.
+ *
+ * https://github.com/anmoroz
+ */
+
+declare(strict_types=1);
 
 namespace App\Controller;
 
--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -1,7 +1,11 @@
 <?php
+/*
+ * This file is part of the Booking application project.
+ *
+ * https://github.com/anmoroz
+ */
 
 declare(strict_types=1);
-
 
 namespace App\Exception;
 
